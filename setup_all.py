f82bc75b3d7d98c7b82bb79709dc3483579f2ae4
Tristan Knoth <tjknoth@gmail.com>
2019-02-28 11:18:14 -0800

    bashrc

--- setup_all.py
+++ setup_all.py
@@ -6,7 +6,7 @@ import platform
 import shutil 
 
 SCRIPT_SOURCE_PATH = "./scripts/"
-I3_SOURCE_PATH = "./"
+SOURCE_PATH = "./"
 I3_CONFIG_FILES = ["config", "i3blocks.conf", "i3status.conf"]
 
 # Set destinations before using script
@@ -41,11 +41,12 @@ if (SCRIPT_DST == "" or CONFIG_DST == ""):
 
 # Copy configs
 for f in I3_CONFIG_FILES:
-    copy(f, I3_SOURCE_PATH, CONFIG_DST)
+    copy(f, SOURCE_PATH, CONFIG_DST)
 
 # Copy scripts
 for f in os.listdir(SCRIPT_SOURCE_PATH):
     copy(f, SCRIPT_SOURCE_PATH, SCRIPT_DST)
 
 # Copy vimrc
-copy(".vimrc",I3_SOURCE_PATH,HOME)
+copy(".vimrc", SOURCE_PATH, HOME)
+copy(".bashrc", SOURCE_PATH, HOME)
